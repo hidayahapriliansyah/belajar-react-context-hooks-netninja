c88eed38b6b558a967c5992735e805789c37d8e7
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-22 11:49:50 +0700

    14-multiple-context-using-hooks

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,24 +1,6 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
-// class BookList extends Component {
-//   static contextType = ThemeContext;
-//   render() { 
-//     const { isLightTheme, light, dark } = this.context;
-//     const theme = isLightTheme ? light : dark;
-
-//     return (
-//       <div className="book-list" style={{  background: theme.bg, color: theme.syntax }}>
-//         <ul>
-//           <li style={{ background: theme.ui }}>the way of king</li>
-//           <li style={{ background: theme.ui }}>the name of the wind</li>
-//           <li style={{ background: theme.ui }}>the final empire</li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
   const theme = isLightTheme ? light : dark;
@@ -34,5 +16,3 @@ const BookList = () => {
 };
 
 export default BookList;
-
-// export default BookList;
