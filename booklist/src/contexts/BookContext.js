59e4744a7ef661177f36f4e68ccfbcffec798881
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-23 05:45:57 +0700

    21-adding-local-storage

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,10 +1,17 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/BookReducer';
 
 export const BooksContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('ninja-context-hook');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('ninja-context-hook', JSON.stringify(books));
+  });
 
   return (
     <BooksContext.Provider value={{ books, dispatch }}>
