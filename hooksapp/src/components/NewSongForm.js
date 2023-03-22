224edc0fb2dfaac5044cb6d78eb3c64454b889f4
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-22 10:52:51 +0700

    11-useState-with-form)

---
+++ hooksapp/src/components/NewSongForm.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const NewSongForm = ({ addSong }) => {
+  const [title, setTitle] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addSong(title);
+    setTitle('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Song name:</label>
+      <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
+      <input type="submit" value="Add song" />
+    </form>
+  );
+}
+
+export default NewSongForm;
--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
   const [songs, setSongs] = useState([
@@ -8,8 +9,8 @@ const SongList = () => {
     { title: 'this wild darkness', id: 3 },
   ]);
 
-  const addSong = () => {
-    setSongs([...songs, { title: 'new song', id: uuidv4() }]);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuidv4() }]);
   }
 
   return (
@@ -21,7 +22,7 @@ const SongList = () => {
           )
         }
       </ul>
-      <button onClick={addSong}>Add a Song</button>
+      <NewSongForm addSong={addSong} />
     </div>
   );
 }
