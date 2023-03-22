8aeb17371af9c31ab13dadbccc966843f69dd33d
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-22 11:33:03 +0700

    13-hooks-with-context

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() { 
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
+// class BookList extends Component {
+//   static contextType = ThemeContext;
+//   render() { 
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
 
-    return (
-      <div className="book-list" style={{  background: theme.bg, color: theme.syntax }}>
-        <ul>
-          <li style={{ background: theme.ui }}>the way of king</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="book-list" style={{  background: theme.bg, color: theme.syntax }}>
+//         <ul>
+//           <li style={{ background: theme.ui }}>the way of king</li>
+//           <li style={{ background: theme.ui }}>the name of the wind</li>
+//           <li style={{ background: theme.ui }}>the final empire</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div className="book-list" style={{  background: theme.bg, color: theme.syntax }}>
+      <ul>
+        <li style={{ background: theme.ui }}>the way of king</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
+
+// export default BookList;
